370f9a152a3f77c2a1771982bb37535244a81acd
lukoninava <126558626+lukoninava@users.noreply.github.com>
2023-02-28 13:12:50 +0400

    Update 11111.py

--- 11111.py
+++ 11111.py
@@ -3,3 +3,10 @@ import numpy as np
 a = np.array([1.2, 2.4, 3.6, 3.5, 6.7, 1.5, 8.4, 3.1, 9.2])
 A = a.reshape(9,1)
 print(A)
+
+
+import numpy as np 
+
+a = np.array([1.2, 2.4, 3.6, 3.5, 6.7, 1.5, 8.4, 3.1, 9.2])
+A = a.reshape(9,1)
+print(A)
